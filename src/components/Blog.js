9cfa931179c367faa7e7a5b4c42cc58790659a7c
Antti H <akthurme@gmail.com>
2022-08-11 14:52:38 +0300

    ->5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+    const [showAll, setShowAll] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return (
+        <div>
+        {
+            showAll === false ?
+            <div style = {blogStyle}>
+                Content: {blog.title} <button type='show' onClick={()=>setShowAll(!showAll)}>view</button>
+            </div> :
+            <div>
+    Author: {blog.author}
+    <div></div>
+    Url: {blog.url}
+    <div></div>
+    Likes: {blog.likes} <button type='like'>like</button>
+    <div></div>
+                <button type='show' onClick={()=>setShowAll(!showAll)}>hide</button>
+            </div>
+        }
+</div>
+    )
+}
+
+
 
 export default Blog
